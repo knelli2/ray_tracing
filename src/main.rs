137ff52e518f3b03aa8d731fd188be4ac9569273
Kyle Nelli <kcnelli21@gmail.com>
2025-12-27 17:12:53 -0600

    Initial pass at writing ppm image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let image_width = 256usize;
+    let image_height = 256usize;
+
+    let output_dir = "/home/knelli/ray_tracing/output/";
+    let filename = "test_image.ppm";
+    let file_path = Path::new(&output_dir).join(filename);
+    let out_file = File::create(file_path).expect("Why can't I create the file");
+    let mut out_buffer = BufWriter::new(out_file);
+
+    writeln!(out_buffer, "P3").expect("Unable to write");
+    writeln!(out_buffer, "{image_width} {image_height}").expect("Unable to write");
+    writeln!(out_buffer, "255").expect("Unable to write");
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            // Not sure why we had to make these doubles first and then turn
+            // them into ints, but I guess I'll leave this from the book
+            // let red = (i as f32) / ((image_width - 1) as f32);
+            // let green = (j as f32) / ((image_height - 1) as f32);
+            // let blue = 0.0;
+
+            // let red = 255.999 * red;
+            // let green = 255.999 * green;
+            // let blue = 255.999 * blue;
+
+            // writeln!(out_buffer, "{} {} {}", red as u32, green as u32, blue
+            // as u32).expect("Unable to write");
+
+            let red = i;
+            let green = j;
+            let blue = 0;
+
+            writeln!(out_buffer, "{} {} {}", red, green, blue).expect("Unable to write");
+        }
+    }
 }
